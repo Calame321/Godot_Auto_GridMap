b64ffb38da7c270c16ac4da58c30cca7e6cfd324
Adam <adamoctav@gmail.com>
2023-02-06 19:56:41 +0200

    update test_with_code

--- test/test_with_code.gd
+++ test/test_with_code.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for x in range(20):
-		for z in range(20):
+	for x in range(10):
+		for z in range(10):
 			grid.set_cell_item(Vector3i(x-10,0,z-10),0)	
 	grid.update_bitmask_region()
